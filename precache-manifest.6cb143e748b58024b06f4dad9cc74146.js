7624f4732425d4927e0985bca8435e54ad464583
vijaysable <vj.sableforever05@gmail.com>
2020-10-24 22:52:00 +0530

    Updates

---
+++ precache-manifest.6cb143e748b58024b06f4dad9cc74146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be1fe773b435df5a8c62cae72b39da0",
+    "url": "/NewsLaundryTask/index.html"
+  },
+  {
+    "revision": "58684f3aacd45b748b6a",
+    "url": "/NewsLaundryTask/static/css/main.d7b55f88.chunk.css"
+  },
+  {
+    "revision": "d350b8fc080ce7abf964",
+    "url": "/NewsLaundryTask/static/js/2.85cab981.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/NewsLaundryTask/static/js/2.85cab981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58684f3aacd45b748b6a",
+    "url": "/NewsLaundryTask/static/js/main.80c29249.chunk.js"
+  },
+  {
+    "revision": "71e5d974f9fdff32504d",
+    "url": "/NewsLaundryTask/static/js/runtime-main.6b1252da.js"
+  }
+]);
